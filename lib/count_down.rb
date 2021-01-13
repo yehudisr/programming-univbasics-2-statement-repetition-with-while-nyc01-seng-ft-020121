dfd870984f990a481576dd2a6bd58fd243afd10c
yehudis raitman <yehudisr@gmail.com>
2021-01-13 16:13:25 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,8 +1,7 @@
 # Write your code here
 count = 10
-magic_exit_number = 0
-while count <= 10 do 
-  break if count == magic_exit_number
+while count >= 1 do 
   puts count
 count-= 1
 end
+puts "Happy New Year!"
